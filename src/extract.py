7304ed347cf284465b4aa23ad1c5da93a56aa14b
KUNG <mathias.boman@gmail.com>
2026-09-02 23:21:35 +0200

    add source field

--- src/extract.py
+++ src/extract.py
@@ -3,13 +3,14 @@ import feedparser
 SVT_RSS_URL = "http://www.svt.se/nyheter/sverige/rss.xml"
 
 
-def extract_rss(url: str) -> list[dict]:
+def extract_rss(url: str, source: str) -> list[dict]:
     feed = feedparser.parse(url)
 
     articles = []
 
     for entry in feed.entries:
         articles.append({
+            "source": source,
             "title": entry.get("title"),
             "description": entry.get("description"),
             "url": entry.get("link"),
