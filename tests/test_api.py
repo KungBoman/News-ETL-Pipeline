b1730dda4355dc6cb1df870c8122177db3d8e6ce
KUNG <mathias.boman@gmail.com>
2026-09-04 04:41:32 +0200

    add test for api

---
+++ tests/test_api.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+
+from src.api import app
+
+client = TestClient(app)
+
+
+def test_root():
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json()["name"] == "Swedish News ETL API"
